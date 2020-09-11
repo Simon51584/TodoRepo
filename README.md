865153c22922236e44f0b5c3f986d833ad4b68b6
Simon51584 <simon51584@gmail.com>
2020-09-11 09:44:45 -0500

    updated and pushed some styling changes

--- README.md
+++ README.md
@@ -7,10 +7,3 @@ rails db:migrate
 
 cd todo-app && npm install
 
-heroku local -f Procfile.dev
-```
-
-To run on Heroku:
-
-``` 
-heroku run rake db:migrate db:seed --app HEROKU_APP
--- todo-app/src/components/About.js
+++ todo-app/src/components/About.js
@@ -6,6 +6,9 @@ class About extends Component {
             <div className="container">
                 <div className="header">
                     <h1>About</h1>
+                    <p>This is a very basic todo application.</p>
+                    <p>Set a task and complete the task.</p>
+                    <p>It's that simple!!!</p>
                 </div>
             </div>
         )
